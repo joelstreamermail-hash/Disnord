2243b1f009259824c012affb3b100b95c9d247ff
joelzimmermann12823wjdmwe-sudo <joelzimmermann12823wjdmwe@gmail.com>
2026-01-13 14:28:09 +0100

    Fix: Cannot GET und CSS Probleme behoben

--- js/server.js
+++ js/server.js
@@ -1,51 +1,25 @@
-const express = require('express');
+﻿const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Statische Ordner freigeben (damit CSS und JS geladen werden)
+// Ordner freigeben (damit HTML die CSS/JS Dateien findet)
 app.use('/css', express.static(path.join(__dirname, '../css')));
 app.use('/js', express.static(path.join(__dirname, '../js')));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
-// --- ALLE ROUTEN DEFINIEREN ---
+// --- ROUTEN ---
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../html/index.html')));
+app.get('/server-create', (req, res) => res.sendFile(path.join(__dirname, '../html/server-create.html')));
+app.get('/direct-messages', (req, res) => res.sendFile(path.join(__dirname, '../html/direct-messages.html')));
+app.get('/settings', (req, res) => res.sendFile(path.join(__dirname, '../html/settings.html')));
 
-// Hauptseite (Home)
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../html/index.html'));
-});
-
-// Login
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '../html/loging.html'));
-});
-
-// NEU: Server erstellen
-app.get('/server-create', (req, res) => {
-    res.sendFile(path.join(__dirname, '../html/server-create.html'));
-});
-
-// NEU: Direktnachrichten
-app.get('/direct-messages', (req, res) => {
-    res.sendFile(path.join(__dirname, '../html/direct-messages.html'));
-});
-
-// NEU: Einstellungen
-app.get('/settings', (req, res) => {
-    res.sendFile(path.join(__dirname, '../html/settings.html'));
-});
-
-// Socket.io Logik
+// Chat Logik
 io.on('connection', (socket) => {
     socket.on('msg', (data) => io.emit('msg', data));
 });
 
-// Server starten
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`✅ Server aktiv: http://localhost:${PORT}`);
-});
+server.listen(3000, () => console.log('✅ SERVER LÄUFT: http://localhost:3000'));
